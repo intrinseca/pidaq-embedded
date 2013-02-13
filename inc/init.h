ac6861d6b47dfaba69f044b7eb42c8ba209a4f4d
Michael Allan <michael@intrinseca.me.uk>
2013-02-13 12:48:29 +0000

    move peripheral init to separate file and general tidy up

---
+++ inc/init.h
@@ -0,0 +1,10 @@
+#ifndef INIT_H_
+#define INIT_H_
+
+void init_rcc();
+void init_gpio();
+void init_usart();
+void init_spi();
+
+
+#endif /* INIT_H_ */
--- src/main.c
+++ src/main.c
@@ -1,15 +1,9 @@
 #include "stm32f10x_conf.h"
 #include "stm32f10x.h"
-#include "stm32f10x_usart.h"
-#include "stm32f10x_gpio.h"
-#include "stm32f10x_spi.h"
-#include "stm32f10x_rcc.h"
+
+#include "init.h"
 
 #define START_SPEED 21
-
-GPIO_InitTypeDef GPIO_InitStructure;
-USART_InitTypeDef USART_InitStructure;
-SPI_InitTypeDef SPI_InitStructure;
 
 void send_usart(char* string) {
 	do {
@@ -39,62 +33,16 @@ void send_spi(char* string, uint16_t length) {
 int main(void) {
 	int i;
 	int j;
-	/*!< At this stage the microcontroller clock setting is already configured,
-	 this is done through SystemInit() function which is called from startup
-	 file (startup_stm32f10x_xx.s) before to branch to application main.
-	 To reconfigure the default setting of SystemInit() function, refer to
-	 system_stm32f10x.c file
-	 */
 
-	/* GPIOD Periph clock enable */
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+	//Initialise Peripherals
+	init_rcc();
+	init_gpio();
+	init_usart();
+	init_spi();
 
-	/* Configure PD0 and PD2 in output pushpull mode */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_15;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-	USART_InitStructure.USART_BaudRate = 115200;
-	USART_InitStructure.USART_HardwareFlowControl =
-			USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-	USART_InitStructure.USART_Parity = USART_Parity_No;
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-	USART_Init(USART1, &USART_InitStructure);
-
-	SPI_StructInit(&SPI_InitStructure);
-	SPI_Init(SPI2, &SPI_InitStructure);
-
+	//Enable SPI and USART
 	SPI_Cmd(SPI2, ENABLE);
 	USART_Cmd(USART1, ENABLE);
-
-	/* To achieve GPIO toggling maximum frequency, the following  sequence is mandatory.
-	 You can monitor PD0 or PD2 on the scope to measure the output signal.
-	 If you need to fine tune this frequency, you can add more GPIO set/reset
-	 cycles to minimize more the infinite loop timing.
-	 This code needs to be compiled with high speed optimization option.  */
 
 	j = 1 << START_SPEED;
 	send_spi("\n\nPiDAQ r1\n", 11);
